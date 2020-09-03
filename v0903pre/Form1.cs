425b3e05b67314a35c64b5a33607bc12a7e99181
Yu Tanaka <YRK00337@nifty.com>
2020-09-03 09:24:46 +0900

    第1弾

--- v0903pre/Form1.cs
+++ v0903pre/Form1.cs
@@ -12,9 +12,22 @@ namespace v0903pre
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int vx = rand.Next(-20, 21);
+        int vy = rand.Next(-20, 21);
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Left = rand.Next(ClientSize.Height - label1.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
